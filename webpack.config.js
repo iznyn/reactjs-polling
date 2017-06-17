bcb5d69a3e6efb8ce19f9328dd41bedf76ac2d78
M Arif Mahthunisnaini <mahthunis@gmail.com>
2017-06-17 13:13:48 +0700

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+/*
+    ./webpack.config.js
+*/
+module.exports = {
+  entry: './app-client.js',
+  output: {
+    filename: 'public/bundle.js'
+  },
+  module: {
+    loaders: [
+            {
+                exclude: /(node_modules|app-server.js)/,
+                loader: 'babel-loader'
+            }
+        ]
+  }
+}
